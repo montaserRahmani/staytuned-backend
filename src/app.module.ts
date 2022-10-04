bdc576b0c85d544de382910489c220aaabaf2e37
montaserRahmani <montaser.rah@gmail.com>
2022-10-04 15:10:59 +0300

    added TypeORM and postgres configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,26 @@ import { ProductModule } from './product/product.module';
 import { NotificationModule } from './notification/notification.module';
 import { SliderModule } from './slider/slider.module';
 import { CategoryModule } from './category/category.module';
-import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule, CategoryModule, SliderModule, NotificationModule],
+  imports: [
+    ProductModule,
+    CategoryModule,
+    SliderModule,
+    NotificationModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123123',
+      database: 'staytuned',
+      entities: [],
+      // For dev purposes, not to be used with production
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
