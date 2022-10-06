09a3368db157a3e3335ba57e4a15e891db0361d5
montaserRahmani <montaser.rah@gmail.com>
2022-10-06 23:49:58 +0300

    added CORS configuration to limit calls to come from certain origin

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ CrudConfigService.load({
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:4200' });
   await app.listen(3000);
 }
 bootstrap();
