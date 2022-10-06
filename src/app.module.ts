fe6a8e7e147f71327ca9f0316e49635d18efeba4
montaserRahmani <montaser.rah@gmail.com>
2022-10-06 16:19:01 +0300

    added AdminJS to have a simple interface to edit entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,23 @@ import { Product } from './product/product.entity';
 import { Slider } from './slider/slider.entity';
 import { Notification } from './notification/notification.entity';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { AdminModule } from '@adminjs/nestjs';
+import AdminJS from 'adminjs';
+import { Database, Resource } from '@adminjs/typeorm';
+
+AdminJS.registerAdapter({ Database, Resource });
+
+const DEFAULT_ADMIN = {
+  email: 'admin@staytuned.com',
+  password: 'stay@tuned!2022',
+};
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
 
 @Module({
   imports: [
@@ -30,6 +47,24 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
       synchronize: true,
     }),
     EventEmitterModule.forRoot(),
+    AdminModule.createAdminAsync({
+      useFactory: () => ({
+        adminJsOptions: {
+          rootPath: '/admin',
+          resources: [Category, Product, Notification, Slider],
+        },
+        auth: {
+          authenticate,
+          cookieName: 'adminjs',
+          cookiePassword: 'secret',
+        },
+        sessionOptions: {
+          resave: true,
+          saveUninitialized: true,
+          secret: 'secret',
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
