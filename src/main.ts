a49a2f79d93d5ae8b496ecb1470b8b38e197bab8
montaserRahmani <montaser.rah@gmail.com>
2022-10-08 00:34:45 +0300

    Added env variable for the frontend client url

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ CrudConfigService.load({
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:4200' });
+  app.enableCors({ origin: process.env.CLIENT_URL });
   await app.listen(3000);
 }
 bootstrap();
