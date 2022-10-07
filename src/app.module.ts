77719fc145c2fd7dbf052dab1563755ca6e99868
montaserRahmani <montaser.rah@gmail.com>
2022-10-07 23:41:53 +0300

    Added dotenv and config service to load db from environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ProductModule } from './product/product.module';
 import { NotificationModule } from './notification/notification.module';
 import { SliderModule } from './slider/slider.module';
 import { CategoryModule } from './category/category.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { Category } from './category/category.entity';
 import { Product } from './product/product.entity';
 import { Slider } from './slider/slider.entity';
@@ -14,6 +14,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AdminModule } from '@adminjs/nestjs';
 import AdminJS from 'adminjs';
 import { Database, Resource } from '@adminjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 AdminJS.registerAdapter({ Database, Resource });
 
@@ -31,20 +32,27 @@ const authenticate = async (email: string, password: string) => {
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ProductModule,
     CategoryModule,
     SliderModule,
     NotificationModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123123',
-      database: 'staytuned',
-      entities: [Category, Product, Notification, Slider],
-      // For dev purposes, not to be used with production
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [Category, Product, Notification, Slider],
+          // For dev purposes, not to be used with production
+          synchronize: configService.get('DB_SYNC') === 'true',
+        } as TypeOrmModuleAsyncOptions;
+      },
     }),
     EventEmitterModule.forRoot(),
     AdminModule.createAdminAsync({
--- src/notification/notification.entity.ts
+++ src/notification/notification.entity.ts
@@ -4,7 +4,7 @@ import { Entity, Column, ManyToOne } from 'typeorm';
 
 @Entity()
 export class Notification extends BaseAuditedEntity {
-  @Column({ type: 'varchar', length: 50 })
+  @Column({ type: 'varchar', length: 50, nullable: true })
   receiverName: string;
 
   @Column({ type: 'varchar', length: 50 })
