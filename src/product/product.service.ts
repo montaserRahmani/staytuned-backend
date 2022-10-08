fa6ed005610f95ac340dc2ac1d5e29643a405b28
montaserRahmani <montaser.rah@gmail.com>
2022-10-09 00:22:07 +0300

    Updated readme file with instructions + removed temp env code

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,15 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Product } from './product.entity';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ProductService extends TypeOrmCrudService<Product> {
-  constructor(
-    @InjectRepository(Product) repo,
-    private configService: ConfigService,
-  ) {
+  constructor(@InjectRepository(Product) repo) {
     super(repo);
-    console.log('=============', this.configService.get<string>('DB_HOST2'));
   }
 }
